3341a0ac38a856a9e46aa43ebf0307316e74605d
Merlin Göttlinger <megoettlinger@gmail.com>
2016-09-11 00:17:12 +0200

    Renamings and signature changes

--- Fractal-Core/src/entities/fractal/Mandelbrot.scala
+++ Fractal-Core/src/entities/fractal/Mandelbrot.scala
@@ -3,23 +3,22 @@ package fractal
 
 object Mandelbrot {
 
-  final class Iterator(val x0: Double, val y0: Double, val maxIteration: Int) { self =>
+  final class Iterator(val x0: Double, val y0: Double, private var iterationsRemaining: Int) { self =>
     type X = Double
     type Y = Double
     var x:X = 0
     var y:Y = 0
     private var xx = x * x
     private var yy = y * y
-    private var iteration = 0
 
-    @inline def hasNext: Boolean = (xx + yy < 4) && iteration < maxIteration
+    @inline def hasNext: Boolean = (xx + yy < 4) && iterationsRemaining >= 0
 
     @inline def next(): Boolean = {
-      xx = x * x
-      yy = y * y
       y = 2 * x * y + y0
       x = xx - yy + x0
-      iteration += 1
+      xx = x * x
+      yy = y * y
+      iterationsRemaining -= 1
       hasNext
     }
 
@@ -38,19 +37,19 @@ object Mandelbrot {
     @inline def foreachY(@inline f: Y => Unit): Unit =
       while (next()) f(y)
 
-    @inline def fold(start: Double)(@inline f: (Double, X, Y) => Double): Double = {
+    @inline def foldLeft(start: Double)(@inline f: (Double, X, Y) => Double): Double = {
       var v = start
       while (next()) v = f(v, x, y)
       v
     }
 
-    @inline def foldX(start: Double)(@inline f: (Double, X) => Double): Double = {
+    @inline def foldLeftX(start: Double)(@inline f: (Double, X) => Double): Double = {
       var v = start
       while (next()) v = f(v, x)
       v
     }
 
-    @inline def foldY(start: Double)(@inline f: (Double, Y) => Double): Double = {
+    @inline def foldLeftY(start: Double)(@inline f: (Double, Y) => Double): Double = {
       var v = start
       while (next()) v = f(v, y)
       v
@@ -90,7 +89,7 @@ object Mandelbrot {
 
   case class OrbitPoint(maxIteration: Int, trapx: Double, trapy: Double) extends Fractal {
     override def apply(x0: Double, y0: Double): Double =
-      new Iterator(x0, y0, maxIteration).fold(1e20) {
+      new Iterator(x0, y0, maxIteration).foldLeft(1e20) {
         (d, x, y) =>
           val (dx, dy) = (x - trapx, y - trapy)
           math.min(d, math.sqrt(dx * dx + dy * dy))
@@ -99,21 +98,21 @@ object Mandelbrot {
 
   case class OrbitImgAxis(maxIteration: Int) extends Fractal {
     override def apply(x0: Double, y0: Double): Double =
-      new Iterator(x0, y0, maxIteration).foldY(1e20) {
+      new Iterator(x0, y0, maxIteration).foldLeftY(1e20) {
         (d, y) => math.min(d, y.abs)
       }
   }
 
   case class OrbitRealAxis(maxIteration: Int) extends Fractal {
     override def apply(x0: Double, y0: Double): Double =
-      new Iterator(x0, y0, maxIteration).foldX(1e20) {
+      new Iterator(x0, y0, maxIteration).foldLeftX(1e20) {
         (d, x) => math.min(d, x.abs)
       }
   }
 
   case class Contour(maxIteration: Int) extends Fractal {
     override def apply(x0: Double, y0: Double): Double =
-      new Iterator(x0, y0, maxIteration).foldX(10d) {
+      new Iterator(x0, y0, maxIteration).foldLeftX(10d) {
         (d, x) => math.max(d, x.abs)
       }
   }

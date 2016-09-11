3f10eeb3b333fe2925d8929723b294005e5a8ea4
Merlin Göttlinger <megoettlinger@gmail.com>
2016-09-11 21:46:41 +0200

    Added a few usefull dimensions

--- Fractal-Core/src/entities/viewport/Dimensions.scala
+++ Fractal-Core/src/entities/viewport/Dimensions.scala
@@ -1,11 +1,13 @@
 package entities.viewport
 
-case class Dimensions(width:Int, height:Int) {
+case class Dimensions(width: Int, height: Int) {
   def scale(factor:Double)= Dimensions((width*factor).toInt, (height*factor).toInt)
 }
 
 object Dimensions {
   val screenHD = Dimensions(1920, 1200)
+  val fullHD = Dimensions(1920, 1080)
+  val lenovoX1 = Dimensions(2560, 1440)
 }
 
 

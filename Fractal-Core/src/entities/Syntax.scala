a0392f85d6dab7fc050ea6f664b9f1736d4e0088
Merlin Göttlinger <megoettlinger@gmail.com>
2016-09-11 21:50:53 +0200

    Refactoring

--- Fractal-Core/src/entities/Syntax.scala
+++ Fractal-Core/src/entities/Syntax.scala
@@ -5,41 +5,45 @@ import entities.color.Color
 import entities.content._
 import entities.image.Image
 
-object Syntax{
-
-  implicit class EnrichedViewport(val viewport:Viewport) extends AnyVal {
-    def withDimensions(dim:Dimensions): Transform = new Transform(viewport, dim)
+object syntax {
+  implicit class EnrichedViewport(val viewport: Viewport) extends AnyVal {
+    def withDimensions(dim: Dimensions): Transform = new Transform(viewport, dim)
   }
 
-  implicit class EnrichedTransform(val transform:Transform) extends AnyVal {
-    def withFractal(fractal:Fractal):Content = FractalContent(fractal, transform)
+  implicit class EnrichedTransform(val transform: Transform) extends AnyVal {
+    def withFractal(fractal: Fractal): Content = FractalContent(fractal, transform)
 
-    def withAntiAliasedFractal(fractal: Fractal, accu: Accumulator = Arithmetic, multi: Int = 5): Content =
-      AntiAliasedFractalContent(fractal, transform, accu, multi)
+    def withAntiAliasedFractal(fractal: Fractal, accu: Accumulator = Arithmetic, samplingFactor: Int = 5): Content =
+      AntiAliasedFractalContent(fractal, transform, accu, samplingFactor)
 
     def withBuddhaBrot(sourceTransform: Transform = transform, maxIteration: Int = 250) =
       BuddahBrot(transform, sourceTransform, maxIteration)
   }
 
-  implicit class EnrichedContent(val content:Content) extends AnyVal{
-    def cached: CachedContent = content match{
-      case cached:CachedContent => cached
+  implicit class EnrichedContent(val content: Content) extends AnyVal {
+    def cached: CachedContent = content match {
+      case cached: CachedContent => cached
       case _ => new CachedContent(content)
     }
-    def linearNormalized: Content with Normalized = LinearNormalizedContent(content.cached)
-    def strongNormalized: Content with Normalized = StrongNormalizedContent(content.cached)
+    def linearNormalized: FinishedContent = LinearNormalizedContent(content.cached)
+    def strongNormalized: FinishedContent = StrongNormalizedContent(content.cached)
   }
 
-  implicit class EnrichedFinishedContent(val content:FinishedContent) extends AnyVal{
-    def withColor(color:Color) = new Image(content, color)
+  implicit class EnrichedFinishedContent(val content: FinishedContent) extends AnyVal {
+    def withColor(color: Color) = new Image(content, color)
   }
 
-  implicit class EnrichedImage(val image:Image) extends AnyVal {
-    def save(file: java.io.File):Unit = {
-      if(file.getParentFile != null)
+  implicit class EnrichedImage(val image: Image) extends AnyVal {
+    def save(file: java.io.File): Unit = {
+      if (file.getParentFile != null)
         file.getParentFile.mkdirs()
       javax.imageio.ImageIO.write(image.buffer, "png", file)
+     }
+    def verboseSave(fileName: String): Unit = {
+      val file = new java.io.File(fileName)
+      save(file)
+      println("Saved: " + file.getAbsoluteFile)
     }
-    def save(fileName:String):Unit = save(new java.io.File(fileName))
+    def save(fileName: String): Unit = save(new java.io.File(fileName))
   }
 }
